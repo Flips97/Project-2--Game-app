9b2cc23ae285ac8fdaf3283bdaa26b7827b340d0
felipe97 <felipe.turen@gmail.com>
2023-12-05 14:59:16 -0800

    finished the oauth

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: 'select_account'
+  }
+))
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/games',
+    failureRedirect: '/games'
+  }
+))
+
+router.get('/logout',function(req, res) {
+  req.logout(function() {
+    res.redirect('/games')
+  })
+})
+
 module.exports = router;
