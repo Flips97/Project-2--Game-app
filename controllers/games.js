6008d67344c3fba983f1af78d9fd98402fc89e8d
felipe97 <felipe.turen@gmail.com>
2023-12-06 11:22:34 -0800

    set the route up for the new form, coded out the controller action, and created a new ejs page

--- controllers/games.js
+++ controllers/games.js
@@ -1,10 +1,15 @@
 const Game = require('../models/games')
 
 module.exports = {
-    index
+    index,
+    new: newGame
 }
 
 async function index(req, res) {
     const games = await Game.find({})
     res.render('games/index', { title: 'All Games', games})
+}
+
+async function newGame(req, res) {
+    res.render('games/new', { title: 'Add Game', errorMsg: '' })
 }
--- routes/games.js
+++ routes/games.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const gamesCtrl = require('../controllers/games')
 
 router.get('/', gamesCtrl.index)
+router.get('/new', gamesCtrl.new)
 
 module.exports = router;
